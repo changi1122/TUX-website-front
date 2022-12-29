7517714528f284bd4849ab5bcd487a55c478b632
hoihoi0101 <chlrudgh5877@gmail.com>
2022-12-30 00:08:28 +0900

    2022-12-30 00:08
    Active page to page

--- src/Page.js
+++ src/Page.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-
+import { useNavigate } from 'react-router-dom';
 const Page = (props) => {
     const {pageId} = useParams();
+    const navigate = useNavigate();
     return (
         <>
             <h3>{pageId}번 페이지</h3>
+            <div>
+                <div onClick={() => navigate(-1)}>뒤로 가기</div>
+                <div onClick={() => navigate(1)}>앞으로 가기</div>
+                <div onClick={() => navigate('/')}>메인으로</div>
+            </div>
         </>
+        
     );
 }
 
