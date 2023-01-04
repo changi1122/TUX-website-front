240e9e47244fa5893c1f4d5c8bdfc1caa54ebafa
yewon0804 <ehdnjs0804@gmail.com>
2023-01-04 15:52:59 +0900

    파일 구조 재구조화, Header.js 업데이트 (#1)
    - 파일 구조 재구조화 내용
    
    	- Add directory src/components, src/pages
    
    - Header.js 업데이트 내용
    
    	- 스크롤 시, 상단 Header가 따라
    
    	- 스타일 변경

---
+++ src/pages/Main.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Main = (props) => {
+	return (
+		<>
+			<h3 className='text-xl font-bold'>메인페이지</h3>
+			<div>
+				<Link className='line_delete' to="/page/1"><h2>1번 페이지</h2></Link>
+				<Link className='line_delete' to="/page/2"><h2>2번 페이지</h2></Link>
+			</div>
+			<div>
+				<br></br>
+				<p>잠깐</p>
+				<p>스크롤 테스트</p>
+				<p>해볼게요</p>
+				<p>ㅁ</p>
+				<p>스크롤 됐을 때</p>
+				<p>아래로 내려도</p>
+				<p>상단 header가</p>
+				<p>잘 따라오면</p>
+				<p>성공</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+				<p>ㅁ</p>
+
+			</div>
+		</>
+	);
+};
+
+export default Main;
