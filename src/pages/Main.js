062eb0ec6c83a5a3d15d79e31d530ed26ab2e7ea
hoihoi0101 <chlrudgh5877@gmail.com>
2023-01-12 22:25:51 +0900

    Add Calender(Library), Post (Component)

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from "swiper/react"; // basic
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import Calendar from 'react-calendar';
+import PostMain from './post/Postmain';
+
+import 'react-calendar/dist/Calendar.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -8,33 +12,58 @@ import 'swiper/css/scrollbar';
 import './Sw.css'
 
 const Main = (props) => {
+  const [value, onChange] = useState(new Date());
+
 	return (
 		<>	
 		<div>
 			<h3 className='text-xl font-bold'>메인페이지</h3>
 			</div>
-			
 			<div>
 			<Swiper className='Banner'
       // install Swiper modules
       modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-	  centeredSlides={true}
-	  centerInsufficientSlides={true}
+      spaceBetween={10}
+	    centeredSlides={true}
+	    centerInsufficientSlides={true}
       slidesPerView={1}
       pagination={{ clickable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
+      onSwiper={(swiper) => console.log(swiper)}  
       onSlideChange={() => console.log('slide change')
 	  
 	}
     >
-      <SwiperSlide className='swiper-slide'><img className="w-10 h-10" src={process.env.PUBLIC_URL + '/logo192.png'} /></SwiperSlide>
-      <SwiperSlide className='swiper-slide'><img className="w-10 h-10" src={process.env.PUBLIC_URL + '/logo192.png'} /></SwiperSlide>
-      <SwiperSlide className='swiper-slide'><img className="w-10 h-10" src={process.env.PUBLIC_URL + '/logo192.png'} /></SwiperSlide>
-      <SwiperSlide className='swiper-slide'><img className="w-10 h-10" src={process.env.PUBLIC_URL + '/logo192.png'} />	</SwiperSlide>
+      <SwiperSlide className='swiper-slide'><img className="w-100 h-80" src={process.env.PUBLIC_URL + '/Slide_dummy.jpg'} /></SwiperSlide>
+      <SwiperSlide className='swiper-slide'><img className="w-100 h-80" src={process.env.PUBLIC_URL + '/Slide_dummy.jpg'} /></SwiperSlide>
+      <SwiperSlide className='swiper-slide'><img className="w-100 h-80" src={process.env.PUBLIC_URL + '/Slide_dummy.jpg'} /></SwiperSlide>
+      <SwiperSlide className='swiper-slide'><img className="w-100 h-80" src={process.env.PUBLIC_URL + '/Slide_dummy.jpg'} />	</SwiperSlide>
       ...
     </Swiper>
     </div>
+    <div style={{padding:40,  flex: 1 , flexDirection: 'column'}}>
+      <div style={{ flex: 1}} >
+        <Calendar
+         onChange={onChange}
+         value={value}
+       />
+      </div>
+
+      <div style={{flex: 1 }}>
+      <div >
+        <PostMain />
+      </div>
+      <div >
+        <PostMain />
+      </div>
+      <div >
+        <PostMain />
+      </div>
+      <div >
+        <PostMain />
+      </div>
+      </div>
+
+      </div>
 		</>
 	);
 };
