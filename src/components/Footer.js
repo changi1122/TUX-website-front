964205a0419f7bd3a62cd8961879e63cb522c7a5
yewon0804 <ehdnjs0804@gmail.com>
2023-01-17 02:49:11 +0900

    Footer 구성 요소 변경 (#3)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,28 +11,17 @@ function Footer(props) {
     return (
         <div className={`w-full flex justify-center bg-[#efefef] mt-auto`}>
             <div className="w-[90%] flex justify-between items-center py-2">
-                <div className="flex items-center gap-6 text-start text-xs">
+                <div className="flex items-center gap-7 text-start text-xs">
                     <div>
-                        <div>
-                            <div className="text-sm">&copy; CBNU TUX</div>
-                            <div>충청북도 청주시 서원구 충대로 1</div>
-                            <div>전자정보대학 3관(S4-1) 108호 TUX</div>
-                        </div>
+                        <div className="text-sm">&copy; CBNU TUX</div>
+                        <div>충청북도 청주시 서원구 충대로 1</div>
+                        <div>전자정보대학 3관(S4-1) 108호 TUX</div>
                     </div>
                     <div>
-                        <div>
-                            <div className="text-sm text-gray-400">가입 문의</div>
-                            <div>(회장) ㅇㅇㅇ 이메일@이메일.com</div>
-                            <div>(부회장) ㅇㅇㅇ 이메일@이메일.com</div>
-                        </div>
-                        <br></br>
-                        <div>
-                            <div className="text-sm text-gray-400">홈페이지 문의</div>
-                            <div>(담당자) ㅇㅇㅇ 이메일@이메일.com</div>
-                        </div>
+                        <a href={process.env.PUBLIC_URL + '/sitemap'} className="text-base">사이트 맵</a>
                     </div>
                 </div>
-                <div className="flex items-center gap-3">
+                <div className="flex items-center gap-5">
                     <div>
                         <div className="relative">
                             {/* position: relative; parent가 child의 기준점이 됨 */}
@@ -57,6 +46,15 @@ function Footer(props) {
                                     </li>
                                     <li>
                                         <a href="https://sw7up.cbnu.ac.kr/" target="_blank">충북대학교<br></br>SW중심대학사업단</a>
+                                    </li>
+                                    <li>
+                                        <a href="https://ece.cbnu.ac.kr/ECE" target="_blank">충북대학교<br></br>전자정보대학</a>
+                                    </li>
+                                    <li>
+                                        <a href="https://cieat.chungbuk.ac.kr/" target="_blank">충북대학교 씨앗</a>
+                                    </li>
+                                    <li>
+                                        <a href="https://cbnul.chungbuk.ac.kr/" target="_blank">충북대학교 도서관</a>
                                     </li>
                                 </ul>
                             </nav>
