2a63deadcd80d2445d384271a155331f4af34287
yewon0804 <ehdnjs0804@gmail.com>
2023-01-28 15:31:22 +0900

    Update RegisterPage: 전체 정규식 추가 (#4)
    - '학번' 입력란 추가

--- src/pages/auth/RegisterPage.js
+++ src/pages/auth/RegisterPage.js
@@ -1,35 +1,37 @@
 import { useCallback, useState } from "react";
 
 function RegisterPage() {
-    // 아이디, 이름, 비밀번호, 비밀번호 확인, 이메일
+    // 아이디, 학번, 이름, 비밀번호, 이메일
     const [userid, setUserid] = useState('');
+    const [studentId, setStudentId] = useState('');
     const [name, setName] = useState('');
     const [userpw, setUserpw] = useState('');
-    const [checkpw, setCheckpw] = useState('');
+    // 비밀번호 확인을 위한 state는 필요 없는 것으로 판단.. onChangeCheckpw() 에서 지역 변수로 처리
     const [email, setEmail] = useState('');
 
     // 오류메시지 상태 저장
     const [useridMessage, setUseridMessage] = useState('');
-    const [nameMessage, setNameMessage] = useState('');
+    const [studentIdMessage, setStudentIdMessage] = useState('');
     const [userpwMessage, setUserpwMessage] = useState('');
     const [checkpwMessage, setCheckpwMessage] = useState('');
     const [emailMessage, setEmailMessage] = useState('');
 
     // 유효성 검사
     const [isUserid, setIsUserid] = useState(false);
-    const [isName, setIsName] = useState(false);
+    const [isStudentId, setIsStudentId] = useState(false);
     const [isUserpw, setIsUserpw] = useState(false);
     const [isCheckpw, setIsCheckpw] = useState(false);
-    const [isEmail, setIsEmail] = useState(false);
+    const [isEmail, setIsEmail] = useState(true); // 필수 입력란이 아니므로
 
+    // userid
     const onChangeUserid = useCallback((e) => {
         const useridRegex = /^(?=.*[a-zA-Z])(?=.*[a-zA-Z0-9]).{2,20}$/
-        const useridRegex2 = /^(?=.*[^a-zA-Z])(?=.*[^a-zA-Z0-9]).{2,20}$/
+        const useridRegex2 = /^(?=.*[^a-zA-Z])(?=.*[^a-zA-Z0-9]).{2,20}$/ // 이거 제외하고 다른 것 들어왔을 때, 에러 처리 하기 위해
         const useridCurrent = e.target.value;
-        setName(useridCurrent);
+        setUserid(useridCurrent);
 
         if (!useridRegex.test(useridCurrent) || useridRegex2.test(useridCurrent)) {
-            setUseridMessage('아이디는 영문자(또는 숫자 포함)의 조합으로 2~20자 작성할 수 있습니다.');
+            setUseridMessage('아이디는 영문자 또는 영문자와 숫자의 조합으로 2~20자 작성해야 합니다.');
             setIsUserid(false);
         }
         else {
@@ -41,29 +43,31 @@ function RegisterPage() {
         }
     }, [])
 
-    // 이메일
-    const onChangeEmail = useCallback((e) => {
-        const emailRegex =
-            /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
-        const emailCurrent = e.target.value;
-        setEmail(emailCurrent);
 
-        if (!emailRegex.test(emailCurrent)) {
-            setEmailMessage('올바르지 않은 이메일 형식입니다.');
-            setIsEmail(false);
-        } else {
-            setEmailMessage('');
-            setIsEmail(true);
+    // student id(학번)
+    const onChangeStudentId = useCallback((e) => {
+        const studentIdRegex = /^(?=.*[0-9]).{2,20}$/
+        const studentIdRegex2 = /^(?=.*[^0-9]).{2,20}$/
+        const studentIdCurrent = e.target.value;
+        setStudentId(studentIdCurrent);
+
+        if (!studentIdRegex.test(studentIdCurrent) || studentIdRegex2.test(studentIdCurrent)) {
+            setStudentIdMessage('올바른 학번 형식이 아닙니다.');
+            setIsStudentId(false);
+        }
+        else {
+            setStudentIdMessage('');
+            setIsStudentId(true);
         }
     }, [])
 
-    const onChangePassword = useCallback((e) => {
-        const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!?*@#$%^+=-])(?=.*[0-9]).{8,30}$/
-        const passwordCurrent = e.target.value;
-        setUserpw(passwordCurrent);
+    const onChangeUserpw = useCallback((e) => {
+        const userpwRegex = /^(?=.*[a-zA-Z])(?=.*[!?*@#$%^&+=-])(?=.*[0-9]).{8,30}$/
+        const userpwCurrent = e.target.value;
+        setUserpw(userpwCurrent);
 
-        if (!passwordRegex.test(passwordCurrent)) {
-            setUserpwMessage('비밀번호는 영문, 숫자, 특수문자(!, @, #, $, %, ^, &, +, =)로 이루어진 8~30자로 입력해 주세요');
+        if (!userpwRegex.test(userpwCurrent)) {
+            setUserpwMessage('비밀번호는 영문, 숫자, 특수문자(!, @, #, $, %, ^, &, +, =)의 조합으로 8~30자 작성해야 합니다.');
             setIsUserpw(false);
         } else {
             setUserpwMessage('');
@@ -72,17 +76,36 @@ function RegisterPage() {
     }, [])
 
     const onChangeCheckpw = useCallback((e) => {
-        const passwordConfirmCurrent = e.target.value;
-        setCheckpw(passwordConfirmCurrent);
+        const checkpwCurrent = e.target.value;
 
-        if (userpw === checkpw) {
+        if (userpw === checkpwCurrent) {
             setCheckpwMessage('');
-            setCheckpwMessage(true);
+            setIsCheckpw(true);
         } else {
-            setCheckpwMessage('입력한 비밀번호와 다릅니다.');
-            setCheckpwMessage(false);
+            setCheckpwMessage('입력하신 비밀번호와 일치하지 않습니다.');
+            setIsCheckpw(false);
         }
     }, [userpw])
+
+    // 이메일
+    const onChangeEmail = useCallback((e) => {
+        const emailRegex =
+            /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+        const emailCurrent = e.target.value;
+        setEmail(emailCurrent);
+
+        if (!emailRegex.test(emailCurrent)) {
+            setEmailMessage('올바른 이메일 형식이 아닙니다.');
+            setIsEmail(false);
+            if (emailCurrent === '') { // 필수 입력란이 아니므로..
+                setEmailMessage('');
+                setIsEmail(true);
+            }
+        } else {
+            setEmailMessage('');
+            setIsEmail(true);
+        }
+    }, [])
 
     return (
         <div className='min-h-[83.99vh] p-20'>
@@ -106,6 +129,7 @@ function RegisterPage() {
                 {/* form */}
                 <form className="mt-3"
                     onSubmit={() => { }}>
+
                     <lebel>
                         <input type="text"
                             name="userid"
@@ -119,13 +143,22 @@ function RegisterPage() {
                     </lebel>
                     <lebel>
                         <input type="text"
+                            name="studentId"
+                            className="border border-x-gray-300 rounded px-4 py-2 w-[92%] mt-3
+                        focus:outline-none focus:ring focus:ring-[#E95420]"
+                            onChange={(e) => { onChangeStudentId(e); }}
+                            placeholder="학번 (20XXXXXXXX)" />
+                        <span className="w-[10%] pl-4 text-[#E95420]">*</span>
+                        <div className={`text-sm text-justify text-[#E95420]`}>{studentIdMessage}</div>
+                    </lebel>
+                    <lebel>
+                        <input type="text"
                             name="name"
                             className="border border-x-gray-300 rounded px-4 py-2 w-[92%] mt-3
                         focus:outline-none focus:ring focus:ring-[#E95420]"
-                            onChange={(e) => { }}
+                            onChange={(e) => { setName(e.target.value); }}
                             placeholder="이름" />
                         <span className="w-[10%] pl-4 text-[#E95420]">*</span>
-                        <div className={`text-sm text-justify text-[#E95420]`}>{nameMessage}</div>
                     </lebel>
 
                     <lebel>
@@ -133,7 +166,7 @@ function RegisterPage() {
                             name="userpw"
                             className="border border-x-gray-300 rounded px-4 py-2 w-[92%] mt-9
                         focus:outline-none focus:ring focus:ring-[#E95420]"
-                            onChange={(e) => { }}
+                            onChange={(e) => { onChangeUserpw(e); }}
                             placeholder="비밀번호(영문, 숫자, 특수문자 8-30자)" />
                         <span className="w-[10%] pl-4 text-[#E95420]">*</span>
                         <div className={`text-sm text-justify text-[#E95420]`}>{userpwMessage}</div>
@@ -143,7 +176,7 @@ function RegisterPage() {
                             name="checkpw"
                             className="border border-x-gray-300 rounded px-4 py-2 w-[92%] mt-3
                         focus:outline-none focus:ring focus:ring-[#E95420]"
-                            onChange={(e) => { }}
+                            onChange={(e) => { onChangeCheckpw(e); }}
                             placeholder="비밀번호 확인" />
                         <span className="w-[10%] pl-4 text-[#E95420]">*</span>
                         <div className={`text-sm text-justify text-[#E95420]`}>{checkpwMessage}</div>
@@ -154,7 +187,7 @@ function RegisterPage() {
                             name="email"
                             className="border border-x-gray-300 rounded px-4 py-2 w-full mt-9
                         focus:outline-none focus:ring focus:ring-[#E95420]"
-                            onChange={(e) => { }}
+                            onChange={(e) => { onChangeEmail(e); }}
                             placeholder="이메일" />
                         <div className={`text-sm text-justify text-[#E95420]`}>{emailMessage}</div>
                     </div>
@@ -166,7 +199,11 @@ function RegisterPage() {
                             onChange={(e) => { }}
                             placeholder="전화번호" />
                     </div>
-                    <button className="bg-[#efefef] hover:bg-gray-200 rounded py-2 w-full mt-6">
+
+                    {/* 버튼 */}
+                    <button className="bg-[#efefef] hover:bg-gray-200 rounded py-2 w-full mt-6
+                        disabled:opacity-75 disabled:hover:bg-[#efefef]"
+                        disabled={!(isUserid && isUserpw && isCheckpw && isEmail)} >
                         회원가입
                     </button>
                     <div className="text-xs mt-3 justify-center flex">
