0496b8776900127b9d57a91dc9d6c7a2390a6921
zino03 <zldnl454@naver.com>
2023-02-01 20:55:12 +0900

    임시로 firebase로 업로드 및 불러오기

--- src/pages/exam/WritePage.js
+++ src/pages/exam/WritePage.js
@@ -1,11 +1,30 @@
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { useState } from 'react';
+import{
+  Link,
+} from "react-router-dom";
+
+import {db} from './fbase'
+import { collection, getDocs, addDoc} from "firebase/firestore";
+//firebase로 임시 작업
 
 //족보 공유를 위한 글쓰기 창인데..
 function Writing(){
+  const [newTitle, setTitle] = useState("");
+  const [newContent, setContent] = useState("");
+  const [newId, setId] = useState(0);
+
+  const usersCollectionRef = collection(db, "users");
+
+  const createContent = async () =>{
+      await addDoc(usersCollectionRef, {title: newTitle, content: newContent, id: newId});
+  }
+
     return (
       <div className="exam-write">
-        <input type='text' placeholder="제목..." size={60}/>
+        <form>
+        <input style={{margin: 10}} type='text' placeholder="제목..." size={60} onChange={(event) => {setTitle(event.target.value)}}/>
         <CKEditor 
           editor={ClassicEditor}
           data=""
@@ -13,11 +32,13 @@ function Writing(){
           }}
 
           onChange={(event, editor) => {
-            const data = editor.getData();
+            const edata = editor.getData().replace(/<[^>]*>?/g, '');
+            {setContent(edata)};
           }}
         />  
+        </form>
         <p>
-          <button>업로드!</button>
+          <button style={{margin: 10}} onClick={createContent}><Link to={"/exam"}>UpLoad</Link></button>
         </p>
       </div>
     );
