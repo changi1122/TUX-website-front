1bf73722a24ac707729d57f625b175d1349ba650
yewon0804 <ehdnjs0804@gmail.com>
2023-02-06 21:19:47 +0900

    Modify Tuxinfo02: 연혁 그래프 추가 (#8)
    - /src/static/jsons/history.json 내용을 자동으로 반영하도록
      - 내용 수정 및 추가 용이

--- src/pages/tuxinfo/Tuxinfo02.js
+++ src/pages/tuxinfo/Tuxinfo02.js
@@ -1,13 +1,41 @@
 import { useState } from "react";
+import './style.css'
+import { history } from "../../static/jsons"
+
+const HistoryBox = ({ props }) => {
+    return (
+        <div>
+            <h1 className="text-2xl">{props.year}</h1>
+            <ol>
+                {
+                    props.contents.map((ele) =>
+                        <li>{"- " + ele}</li>
+                    )
+                }
+            </ol>
+        </div>
+    )
+}
 
 function Tuxinfo02() {
     return (
-        <div className='min-h-screen md:p-20 px-3 py-10'>
-            TUX개요 - 연혁
+        <div className='min-h-screen px-3 md:py-20 py-10'>
+            <div className="border-b border-black w-full md:pb-20 pb-10">
+                <div className="text-lg">TUX개요</div>
+                <div className="text-4xl font-bold">연혁</div>
+            </div>
 
-            <div>
-                동아리 설립 일자..
-                수상 실적 같은 것들..
+            <div className="mt-20 text-left md:px-[20vw] px-0">
+                <div>
+                    <div className="text-3xl text-black">TUX HISTORY</div>
+                    <p>주요 활동과 실적들</p>
+                </div>
+
+                <div className="history mt-10">
+                    {
+                        history.map((ele) => <HistoryBox key={ele.year} props={ele} />)
+                    }
+                </div>
             </div>
         </div>
     );
--- src/static/jsons/index.js
+++ src/static/jsons/index.js
@@ -1,5 +1,6 @@
 import gnbIsLogin from "./gnbIsLogin.json"
 import gnbIsNotLogin from "./gnbIsNotLogin.json"
 import relatedSites from "./relatedSites.json"
+import history from "./history.json"
 
-export { gnbIsLogin, gnbIsNotLogin, relatedSites };
+export { gnbIsLogin, gnbIsNotLogin, relatedSites, history };
