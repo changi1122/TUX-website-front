5553a130519827c8630c60f7822511ad542fca96
yewon0804 <ehdnjs0804@gmail.com>
2023-02-07 04:08:34 +0900

    Sitemap: 컨플릭트 해결, 디자인 변경

--- src/pages/Sitemap.js
+++ src/pages/Sitemap.js
@@ -1,6 +1,4 @@
 import { useState } from "react";
-<<<<<<< HEAD
-=======
 import { BsQuestionCircleFill } from 'react-icons/bs';
 import { MdDeveloperMode } from 'react-icons/md';
 import { gnbIsLogin, gnbIsNotLogin } from "../static/jsons"
@@ -42,48 +40,46 @@ const GnbBox = ({ gnb, isLogin }) => {
         </div>
     )
 };
->>>>>>> 1bf73722a24ac707729d57f625b175d1349ba650
 
 function Sitemap() {
     const [isLogin, setIsLogin] = useState(true);
     // localStorage로부터 user info를 받아와서 처리해야 하는듯 하지만? 일단 간단하게 처리
 
     return (
-        <div className='min-h-screen md:p-20 px-3 py-10'>
-            <div>
-                <div className='text-xl font-black'>CBNU TUX</div>
-                <div className="text-xl">사이트 맵</div>
+        <div className='min-h-screen px-3 md:py-20 py-10'>
+            <div className="border-b border-black w-full md:pb-20 pb-10">
+                <div className="text-lg">CBNU TUX</div>
+                <div className="text-4xl font-bold">사이트 맵</div>
             </div>
 
-<<<<<<< HEAD
-=======
-            <div className="mt-10 flex md:flex-row flex-col gap-3 md:justify-center items-start">
-                {
-                    gnbIsLogin.map((ele) => <GnbBox key={ele.gnbName} gnb={ele} isLogin={isLogin} />)
-                }
-            </div>
-
-            <div className="w-full flex md:flex-row flex-col justify-between mt-20 gap-8">
-                <div className="">
-                    <BsQuestionCircleFill size={20} style={{ margin: 'auto auto auto 0' }} />
-                    <div className="text-left mt-2">
-                        <p>사이트 맵이란?</p>
-                        <p className="text-justify">사이트에 표시할 콘텐츠들을 한 눈에 알아볼 수 있도록, 메뉴별로 구분하여 설계한 것을 말합니다.</p>
-                    </div>
+            <div className="mt-20 md:px-20 px-0">
+                <div className="flex md:flex-row flex-col gap-3 md:justify-center items-start">
+                    {
+                        gnbIsLogin.map((ele) => <GnbBox key={ele.gnbName} gnb={ele} isLogin={isLogin} />)
+                    }
                 </div>
 
-                <div className="w-[300px] md:ml-0 ml-auto md:mt-0 mt-10">
-                    <MdDeveloperMode size={30} style={{ margin: 'auto 0 auto auto' }} color="rgb(100 116 139)" />
-                    <div className="text-right mt-2 text-slate-500">
-                        <p>_Developers</p>
-                        <p>Front-End</p>
-                        <p className="">김예원, 변진호, 최경호</p>
-                        <p>Back-End</p>
-                        <p className="">오지우, 임수연, 차현아</p>
+                <div className="w-full flex md:flex-row flex-col justify-between mt-20 gap-8">
+                    <div className="">
+                        <BsQuestionCircleFill size={20} style={{ margin: 'auto auto auto 0' }} />
+                        <div className="text-left mt-2">
+                            <p>사이트 맵이란?</p>
+                            <p className="text-justify">사이트에 표시할 콘텐츠들을 한 눈에 알아볼 수 있도록, 메뉴별로 구분하여 설계한 것을 말합니다.</p>
+                        </div>
+                    </div>
+
+                    <div className="w-[300px] md:ml-0 ml-auto md:mt-0 mt-10">
+                        <MdDeveloperMode size={30} style={{ margin: 'auto 0 auto auto' }} color="rgb(100 116 139)" />
+                        <div className="text-right mt-2 text-slate-500">
+                            <p>_Developers</p>
+                            <p>Front-End</p>
+                            <p className="">김예원, 변진호, 최경호</p>
+                            <p>Back-End</p>
+                            <p className="">오지우, 임수연, 차현아</p>
+                        </div>
                     </div>
                 </div>
             </div>
->>>>>>> 1bf73722a24ac707729d57f625b175d1349ba650
         </div>
     );
 }
