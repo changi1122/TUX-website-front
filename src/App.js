3beb1db14db6e946d275bd03a000679bcef0d145
Woochang Lee <changi112242@outlook.com>
2023-08-13 22:44:57 +0900

    Fix: React.Suspense 추가

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import './App.css';
 
 import React, { lazy, useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import NotFound from './NotFound';
 
 import { Header, Footer, PrivateRoute } from './components';
+import Loading from './Loading';
+import NotFound from './NotFound';
 
 /* Lazy loading for code splitting */
 const Main = lazy(() => import('./pages/Main'));
@@ -82,85 +83,82 @@ const App = () => {
       <BrowserRouter>
         <Header isLogin={isLogin} setIsLogin={setIsLogin} />
 
-        <Routes>
-          <Route path="/" element={
-            // 로그인 한 사용자의 id가 'admin'일 경우, 관리자 페이지로 route
-            <PrivateRoute isThatTrue={isAdmin} isTrue={<AdministratorPage />} isFalse={<Main />} />
-          } />
-          <Route path="/sitemap" element={<Sitemap isLogin={isLogin} />}></Route>
+        <React.Suspense fallback={Loading}>
+          <Routes>
+            <Route path="/" element={
+              // 로그인 한 사용자의 id가 'admin'일 경우, 관리자 페이지로 route
+              <PrivateRoute isThatTrue={isAdmin} isTrue={<AdministratorPage />} isFalse={<Main />} />
+            } />
+            <Route path="/sitemap" element={<Sitemap isLogin={isLogin} />}></Route>
 
 
-          {/* auth pages */}
-          <Route path="/login" element={
-            // 이미 로그인 되어 있는 상태라면, login page에 접근 불가 -> NotFound page로 route
-            <PrivateRoute isThatTrue={isLogined()} isTrue={<NotFound />} isFalse={<LoginPage isLogin={isLogin} setIsLogin={setIsLogin} />} />
-          } />
-          <Route path="/signup" element={
-            <PrivateRoute isThatTrue={isLogined()} isTrue={<NotFound />} isFalse={<RegisterPage />} />
-          } />
-          <Route path="/signup/successful" element={
-            <PrivateRoute isThatTrue={isLogined()} isTrue={<NotFound />} isFalse={<SuccessfulSignup />} />
-          } />
-          <Route path="/mypage" element={
-            // 로그인 하지 않은 사용자는, mypage에 접근할 수 없음
-            <PrivateRoute isThatTrue={isLogined()} isTrue={<MyPage />} isFalse={<NotFound />} />
-          } />
+            {/* auth pages */}
+            <Route path="/login" element={
+              // 이미 로그인 되어 있는 상태라면, login page에 접근 불가 -> NotFound page로 route
+              <PrivateRoute isThatTrue={isLogined()} isTrue={<NotFound />} isFalse={<LoginPage isLogin={isLogin} setIsLogin={setIsLogin} />} />
+            } />
+            <Route path="/signup" element={
+              <PrivateRoute isThatTrue={isLogined()} isTrue={<NotFound />} isFalse={<RegisterPage />} />
+            } />
+            <Route path="/signup/successful" element={
+              <PrivateRoute isThatTrue={isLogined()} isTrue={<NotFound />} isFalse={<SuccessfulSignup />} />
+            } />
+            <Route path="/mypage" element={
+              // 로그인 하지 않은 사용자는, mypage에 접근할 수 없음
+              <PrivateRoute isThatTrue={isLogined()} isTrue={<MyPage />} isFalse={<NotFound />} />
+            } />
 
 
-          {/* TUX 개요 */}
-          <Route path="/tuxinfo01" element={<Tuxinfo01 />}></Route>
-          <Route path="/tuxinfo02" element={<Tuxinfo02 />}></Route>
-          <Route path="/tuxinfo03" element={<Tuxinfo03 />}></Route>
+            {/* TUX 개요 */}
+            <Route path="/tuxinfo01" element={<Tuxinfo01 />}></Route>
+            <Route path="/tuxinfo02" element={<Tuxinfo02 />}></Route>
+            <Route path="/tuxinfo03" element={<Tuxinfo03 />}></Route>
 
 
-          {/* 커뮤니티 */}
-          <Route path="/community" element={<Community />}></Route>
-          <Route path="/community/:id" element={<CommunityDetail />}></Route>
-          <Route path="/community/write" element={<CommunityWrite />}></Route>
+            {/* 커뮤니티 */}
+            <Route path="/community" element={<Community />}></Route>
+            <Route path="/community/:id" element={<CommunityDetail />}></Route>
+            <Route path="/community/write" element={<CommunityWrite />}></Route>
 
 
-          {/* 자료실 */}
-          <Route path="/referenceroom" element={
-            <PrivateRoute isThatTrue={isLogined() && isNotGuest()} isTrue={<ReferenceRoom />} isFalse={<Navigate to='/login' />} />
-          } />
-          <Route path="/referenceroom/:id" element={
-            <PrivateRoute isThatTrue={isLogined() && isNotGuest()} isTrue={<ReferenceRoomDetail />} isFalse={<Navigate to='/login' />} />
-          } />
-          <Route path="/referenceroom/write" element={
-            <PrivateRoute isThatTrue={isLogined() && isNotGuest()} isTrue={<ReferenceRoomWrite />} isFalse={<Navigate to='/login' />} />
-          } />
+            {/* 자료실 */}
+            <Route path="/referenceroom" element={
+              <PrivateRoute isThatTrue={isLogined() && isNotGuest()} isTrue={<ReferenceRoom />} isFalse={<Navigate to='/login' />} />
+            } />
+            <Route path="/referenceroom/:id" element={
+              <PrivateRoute isThatTrue={isLogined() && isNotGuest()} isTrue={<ReferenceRoomDetail />} isFalse={<Navigate to='/login' />} />
+            } />
+            <Route path="/referenceroom/write" element={
+              <PrivateRoute isThatTrue={isLogined() && isNotGuest()} isTrue={<ReferenceRoomWrite />} isFalse={<Navigate to='/login' />} />
+            } />
 
-          {/* 갤러리 */}
-          <Route path="/gallery" element={
-            <PrivateRoute isThatTrue={isLogined() && isNotGuest()} isTrue={<Gallery />} isFalse={<Navigate to='/login' />} />
-          } />
-          <Route path="/gallery/:id" element={
-            <PrivateRoute isThatTrue={isLogined() && isNotGuest()} isTrue={<GalleryDetail />} isFalse={<Navigate to='/login' />} />
-          } />
-          <Route path="/gallery/write" element={
-            <PrivateRoute isThatTrue={isLogined() && isNotGuest()} isTrue={<GalleryWrite />} isFalse={<Navigate to='/login' />} />
-          } />
+            {/* 갤러리 */}
+            <Route path="/gallery" element={
+              <PrivateRoute isThatTrue={isLogined() && isNotGuest()} isTrue={<Gallery />} isFalse={<Navigate to='/login' />} />
+            } />
+            <Route path="/gallery/:id" element={
+              <PrivateRoute isThatTrue={isLogined() && isNotGuest()} isTrue={<GalleryDetail />} isFalse={<Navigate to='/login' />} />
+            } />
+            <Route path="/gallery/write" element={
+              <PrivateRoute isThatTrue={isLogined() && isNotGuest()} isTrue={<GalleryWrite />} isFalse={<Navigate to='/login' />} />
+            } />
 
-          {/* 지원하기 */}
-          <Route path="/join" element={<JoinPage />}></Route>
-          <Route path="/contact" element={<ContactPage />}></Route>
+            {/* 지원하기 */}
+            <Route path="/join" element={<JoinPage />}></Route>
+            <Route path="/contact" element={<ContactPage />}></Route>
 
-          {/* 사용안함: <Route path='/postView/:no' component={<PostView />} />*/}
+            {/* 사용안함: <Route path='/postView/:no' component={<PostView />} />*/}
 
-          {/* 엘리먼트의 상단에 위치하는 라우트들의 규칙을 모두 확인하고, 일치하는 라우트가 없다면 이 라우트가 화면에 나타나게 됩니다. */}
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
+            {/* 엘리먼트의 상단에 위치하는 라우트들의 규칙을 모두 확인하고, 일치하는 라우트가 없다면 이 라우트가 화면에 나타나게 됩니다. */}
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
 
-
-
-        <Footer />
+          <Footer />
+        </React.Suspense>
       </BrowserRouter>
     </div>
   );
 };
-
-
-
 
 
 export default App;
