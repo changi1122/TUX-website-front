74eaa801929cccbe54901bfc6178ad6623fe0c55
Woochang Lee <changi112242@outlook.com>
2024-01-11 17:41:40 +0900

    Update: 회원 탈퇴 추가

--- src/pages/auth/MyPage.js
+++ src/pages/auth/MyPage.js
@@ -100,6 +100,39 @@ function MyPage() {
         }
     }
 
+    // 회원 탈퇴
+    async function deleteUser() {
+        if (window.confirm("정말로 회원 탈퇴하시겠습니까?")) {
+            const typeUsername = prompt("회원 탈퇴하면 현재 사용 중인 아이디를 다시 사용할 수 없습니다.\n회원 탈퇴를 위해 현재 사용 중인 아이디를 입력하세요.");
+            if (typeUsername === user.username) {
+                const res = await fetch(`/api/user/${user.id}`, {
+                    method: "DELETE",
+                    credentials: 'include'
+                });
+
+                if (res.ok) {
+                    localStorage.removeItem('cbnu_tux_userid');
+                    sessionStorage.removeItem('cbnu_tux_userid');
+                    localStorage.removeItem('userId');
+                    sessionStorage.removeItem('userId');
+                    localStorage.removeItem('username');
+                    sessionStorage.removeItem('username');
+                    localStorage.removeItem('role');
+                    sessionStorage.removeItem('role');
+                    localStorage.removeItem('nickname');
+                    sessionStorage.removeItem('nickname');
+                    localStorage.removeItem('expire');
+                    navigate('/');
+                    window.location.reload();
+                } else {
+                    alert("회원 탈퇴 중 오류가 발생하였습니다.");
+                }
+            } else {
+                alert("입력하신 아이디가 일치하지 않습니다.");
+            }
+        }
+    }
+
     return (
         <div className='min-h-screen mx-auto lg:w-[936px] w-full text-left px-3 md:pt-10 md:pb-20 pt-5 pb-10'>
             <h2 className='text-2xl mb-4'>마이페이지</h2>
@@ -108,12 +141,12 @@ function MyPage() {
                     <div>
                         <label className="block mb-2 text-sm font-medium text-gray-900">아이디</label>
                         <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                            placeholder={user.username} disabled readonly/>
+                            value={user.username} disabled readOnly/>
                     </div>
                     <div>
                         <label className="block mb-2 text-sm font-medium text-gray-900">비밀번호</label>
                         <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                            placeholder="BCrypt 해싱으로 암호화되어 저장됨" disabled readonly/>
+                            placeholder="BCrypt 해싱으로 암호화되어 저장됨" disabled readOnly/>
                         <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 w-full my-1 inline-block"
                             onClick={(e) => { e.preventDefault(); handleChangePassword(); }}>
                             비밀번호변경
@@ -122,7 +155,7 @@ function MyPage() {
                     <div>
                         <label className="block mb-2 text-sm font-medium text-gray-900">닉네임</label>
                         <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                            placeholder={user.nickname} disabled readonly/>
+                            value={user.nickname} disabled readOnly/>
                         <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 w-full my-1 inline-block"
                             onClick={(e) => { e.preventDefault(); handleChangeNickname(); }}>
                             닉네임 변경
@@ -131,12 +164,12 @@ function MyPage() {
                     <div>
                         <label className="block mb-2 text-sm font-medium text-gray-900">회원 권한</label>
                         <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                            placeholder={user.role} disabled readonly/>
+                            value={user.role} disabled readOnly/>
                     </div>
                     <div>
                         <label className="block mb-2 text-sm font-medium text-gray-900">이메일</label>
                         <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                            placeholder={user.email} disabled readonly/>
+                            value={user.email} disabled readOnly/>
                         <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 w-full my-1 inline-block"
                             onClick={(e) => { e.preventDefault(); handleChangeEmail(); }}>
                             이메일 변경
@@ -145,7 +178,7 @@ function MyPage() {
                     <div>
                         <label className="block mb-2 text-sm font-medium text-gray-900">전화번호</label>
                         <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                            placeholder={user.phoneNumber} disabled readonly/>
+                            value={user.phoneNumber} disabled readOnly/>
                         <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 w-full my-1 inline-block"
                             onClick={(e) => { e.preventDefault(); handleChangePhoneNumber(); }}>
                             전화번호 변경
@@ -154,7 +187,7 @@ function MyPage() {
                     <div>
                         <label className="block mb-2 text-sm font-medium text-gray-900">학과</label>
                         <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                            placeholder={user.department} disabled readonly/>
+                            value={user.department} disabled readOnly/>
                         <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 w-full my-1 inline-block"
                             onClick={(e) => { e.preventDefault(); handleChangeDepartment(); }}>
                             학과 변경
@@ -163,8 +196,14 @@ function MyPage() {
                     <div>
                         <label className="block mb-2 text-sm font-medium text-gray-900">학번</label>
                         <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                            placeholder={user.studentNumber} disabled readonly/>
+                            value={user.studentNumber} disabled readOnly/>
                     </div>
+                </div>
+                <div className='text-right'>
+                    <button className="text-red-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 w-36 my-1 inline-block"
+                        onClick={(e) => { e.preventDefault(); deleteUser(); }}>
+                        회원 탈퇴
+                    </button>
                 </div>
             </form>
         </div>
